734c07a2500df8fbb86ffecfa611df8ea24d06fe
robwri32 <robwri32@gmail.com>
2015-09-29 10:47:59 -0400

    added in csvs for beers and locations 153 in total

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'csv'
+
+CSV.foreach("db/beers.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+  Beer.create(row.to_hash)
+end
+
+CSV.foreach("db/locations.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+  Location.create(row.to_hash)
+end
